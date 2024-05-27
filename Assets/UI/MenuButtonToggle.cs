c744c7a70b9fac503d5da35530207c32832d4fac
OveBK <oviwan257@gmail.com>
2024-05-27 13:13:56 +0200

    Fix and edit UI

---
+++ Assets/UI/MenuButtonToggle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonScript : MonoBehaviour
+{
+    public GameObject optionsWindow;
+
+    // Method to toggle the options window
+    public void ToggleOptionsWindow()
+    {
+        if (optionsWindow != null)
+        {
+            bool isActive = optionsWindow.activeSelf;
+            optionsWindow.SetActive(!isActive);
+        }
+    }
+}
