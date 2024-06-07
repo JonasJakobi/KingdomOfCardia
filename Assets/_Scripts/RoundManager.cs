bdba9cfbf3c1491160a50d23655e05b051ab7572
OveBK <oviwan257@gmail.com>
2024-06-07 05:17:47 +0200

    Assign enemy spawn to SpawnPoints

--- Assets/_Scripts/RoundManager.cs
+++ Assets/_Scripts/RoundManager.cs
@@ -5,8 +5,22 @@ using com.cyborgAssets.inspectorButtonPro;
 using UnityEngine.UI;
 //RoundManager to determine how many and which enemies are present in a round.
 
+public class SpawnPoint
+{
+    public int widthPosition;
+    public int heightPosition;
+    public bool isFullWidth;
+
+    public SpawnPoint(int widthPos, int heightPos, bool onFullWidth)
+    {
+        widthPosition = widthPos;
+        heightPosition = heightPos;
+        isFullWidth = onFullWidth;
+    }
+}
 public class RoundManager : Singleton<RoundManager>
 {
+    [SerializeField] private List<SpawnPoint> spawnPoints = new List<SpawnPoint>();
     public Button startRoundButton;
     [Header("Round Statistics")]
     public int round = 1;
@@ -21,6 +35,7 @@ public class RoundManager : Singleton<RoundManager>
     [SerializeField] private GameObject EnemyType3;
 
     [Header("Spawn behaviour")]
+    public int maxSpawnPoints = 3;
     public float minSpawnTime = 1.0f;
     public float maxSpawnTime = 5.0f;
 
@@ -36,6 +51,7 @@ public class RoundManager : Singleton<RoundManager>
     {
         startRoundButton.onClick.AddListener(changeToPlayMode);
         roundValueLeft = 0;
+        CreateSpawnPoints(1);
     }
 
     private void Update()
@@ -87,13 +103,12 @@ public class RoundManager : Singleton<RoundManager>
     //Spawn an enemy
     private void SpawnEnemy(int enemyValue)
     {
-
-        int width = GridManager.WIDTH - 1;
-        int height = GridManager.HEIGHT;
         GameObject enemyPrefab = null;
-        int[] spawnWidth = { 0, width };
-        int randomIndex = Random.Range(0, 2);
-        int randomWidth = spawnWidth[randomIndex];
+        int randomIndex = Random.Range(0, spawnPoints.Count);
+        float randomDistance = Random.Range(-1.0f, 1.0f);
+        SpawnPoint spawnPoint = spawnPoints[randomIndex];
+        int width = GridManager.WIDTH - 1;
+        int height = GridManager.HEIGHT - 1;
 
         switch (enemyValue)
         {
@@ -111,8 +126,55 @@ public class RoundManager : Singleton<RoundManager>
                 return;
         }
 
-        Vector3 randomPos = new Vector3(randomWidth, Random.Range(0, height), 0);
-        GameObject newEnemy = Instantiate(enemyPrefab, randomPos, Quaternion.identity);
+        if (spawnPoint.isFullWidth)
+        {
+            if ((spawnPoint.widthPosition + randomDistance) >= width || (spawnPoint.widthPosition + randomDistance) <= 0)
+            {
+                randomDistance = -randomDistance;
+            }
+            Vector3 randomPos = new Vector3(spawnPoint.widthPosition + randomDistance, spawnPoint.heightPosition, 0);
+            GameObject newEnemy = Instantiate(enemyPrefab, randomPos, Quaternion.identity);
+        }
+        else
+        {
+            if ((spawnPoint.heightPosition + randomDistance) >= height || (spawnPoint.heightPosition + randomDistance) <= 0)
+            {
+                randomDistance = -randomDistance;
+            }
+            Vector3 randomPos = new Vector3(spawnPoint.widthPosition, spawnPoint.heightPosition + randomDistance, 0);
+            GameObject newEnemy = Instantiate(enemyPrefab, randomPos, Quaternion.identity);
+        }
+    }
+
+    private void CreateSpawnPoints(int amount)
+    {
+        int width = GridManager.WIDTH - 1;
+        int height = GridManager.HEIGHT - 1;
+        int[] removeMiddleWidth = { 0, width };
+        int[] removeMiddleHeight = { 0, height };
+
+        spawnPoints.Clear();
+
+        for (int i = 0; i < amount; i++)
+        {
+            bool onFullWidth = (Random.Range(0, 2) == 0);
+            int randomIndex = Random.Range(0, 2);
+            int randomHeight;
+            int randomWidth;
+
+            if (onFullWidth)
+            {
+                randomHeight = removeMiddleHeight[randomIndex];
+                randomWidth = Random.Range(0, width);
+            }
+            else
+            {
+                randomWidth = removeMiddleWidth[randomIndex];
+                randomHeight = Random.Range(0, height);
+            }
+            spawnPoints.Add(new SpawnPoint(randomWidth, randomHeight, onFullWidth));
+            Debug.Log("Mit " + randomWidth + ", " + randomHeight + ", " + onFullWidth);
+        }
     }
 
     private void QueueWave()
@@ -130,6 +192,14 @@ public class RoundManager : Singleton<RoundManager>
         UIChangeManager.Instance.updateRound();
         enemyCount = 0;
         roundValue = roundValue * 2;
+        if (round <= 3)
+        {
+            CreateSpawnPoints(1);
+        }
+        else
+        {
+            CreateSpawnPoints(Random.Range(1, maxSpawnPoints));
+        }
     }
 
     //Start next round and decide if a wave will be present
