cb6702f74a262f3055600ba6e5e4c7773dae8144
Jonas Jakobi <93149084+JonasJakobi@users.noreply.github.com>
2024-07-17 15:16:07 +0200

    Game Difficulties creatable

--- Assets/_Scripts/Utilities/Constants.cs
+++ Assets/_Scripts/Utilities/Constants.cs
@@ -31,11 +31,3 @@ public class Constants : Singleton<Constants>
 }
 
 
-[CreateAssetMenu(fileName = "Constants", menuName = "Constants", order = 0)]
-public class GameDifficulty : ScriptableObject
-{
-    public float towerCostGrowthRate = 1.7f;
-    public float damageToEnemiesMultiplier = 1.0f;
-    public float damageToTowersMultiplier = 1.0f;
-    public float enemyMoveSpeedMultiplier = 1.0f;
-}
---
+++ Assets/_Scripts/Utilities/GameDifficulty.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameDifficulty", menuName = "GameDifficulty", order = 0)]
+public class GameDifficulty : ScriptableObject
+{
+    public float towerCostGrowthRate = 1.7f;
+    public float damageToEnemiesMultiplier = 1.0f;
+    public float damageToTowersMultiplier = 1.0f;
+    public float enemyMoveSpeedMultiplier = 1.0f;
+}
