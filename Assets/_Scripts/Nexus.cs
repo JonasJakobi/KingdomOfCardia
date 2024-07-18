30ab3560023477cc382df131d960f89964ee712f
Jonas Jakobi <93149084+JonasJakobi@users.noreply.github.com>
2024-07-18 16:06:23 +0200

    Nexus can damage around it, some balancing

--- Assets/_Scripts/Nexus.cs
+++ Assets/_Scripts/Nexus.cs
@@ -6,19 +6,31 @@ using UnityEngine;
 public class Nexus : Singleton<Nexus>
 {
     [SerializeField] private int health = 100;
-
-
-
-
-
-    public void TakeDamage(int damage)
+    int damagingIndex = 0;
+    private void FixedUpdate()
     {
-        health -= damage;
-        if (health <= 0)
+        damagingIndex++;
+        if (damagingIndex >= 10)
         {
-            // Game over
-            GameManager.Instance.ChangeGameState(GameState.GameOver);
-            Debug.Log("Game Over Nexus!");
+            damagingIndex = 0;
+            DealDamage();
         }
+
+
+    }
+
+    private void DealDamage()
+    {
+        //check adjacent fields for enemies, deal some damage to them
+        BaseTower baseTower = GetComponent<BaseTower>();
+        var adjacentTiles = GridManager.Instance.GetNeighbours(GridManager.Instance.GetTileAtPosition(transform.position));
+        foreach (var tile in adjacentTiles)
+        {
+            if (tile.enemies.Count > 0)
+            {
+                tile.enemies.ForEach(e => e.TakeDamage(1));
+            }
+        }
+        GridManager.Instance.GetTileAtPosition(transform.position).enemies.ForEach(e => e.TakeDamage(1));
     }
 }
